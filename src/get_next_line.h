5ae2c94a5f8545efddb5c1a6baf9768cd45f6bc4
Arthur Poulet <poulet_a@epitech.eu>
2014-03-02 21:50:20 +0100

    up

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -1,11 +1,11 @@
 /*
-** get_next_line.h for get_next_line in /home/ghukas_g/rendu/get_next_line-2018-ghukas_g
+** get_next_line.h for get_next_line in /home/ghukas_g/rendu/gnl-2018-ghukas_g
 **
 ** Made by ghukas_g
 ** Login   <ghukas_g@epitech.net>
 **
 ** Started on  Sat Nov 16 17:19:38 2013 ghukas_g
-** Last update Fri Feb 28 22:05:08 2014 ghukas_g
+** Last update Sun Mar  2 21:47:03 2014 poulet_a
 */
 
 #ifndef GET_NEXT_LINE_H_
